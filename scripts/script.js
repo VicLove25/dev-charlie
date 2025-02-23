d1b961cb60abc18f42f8bfbf3ec999adf538b7e3
VicLove25 <vlove@una.edu>
2025-02-23 13:28:57 -0600

    script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // dongo
-@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
+// some js
+$( document ).ready(function() {
+    console.log( "hey cumbie, don't click on the seal" );
+
+    $('#classMascot').hover(
+
+        function(){
+            $(this).css("background-color", "yellow");
+            alert('never use alert');
+            }
+    ); 
+    
+});
