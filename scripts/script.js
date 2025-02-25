368787d9c1ca4c2d5c8d833c672cb7d1d926fed4
VicLove25 <vlove@una.edu>
2025-02-24 23:07:01 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 // dongo
 // some js
 $( document ).ready(function() {
-    console.log( "hey cumbie, don't click on the seal" );
+    console.log( "don't click on the seal" );
 
     $('#classMascot').hover(
 
         function(){
-            $(this).css("background-color", "yellow");
+            $(this).css("background-color", "red");
             alert('never use alert');
             }
     ); 
